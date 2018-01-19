c524ee808b04cc9709044307fa639a5997d2097a
Jennifer Dobak <jdobak1@gmail.com>
2018-01-19 17:59:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -18,8 +18,7 @@ prime?(-1)
 
 def prime?(number)
   range = (2..number-1).to_a
-  range2 = (-number+1..-2).to_a
-  if range.all? {|x| number % x != 0} && range2.all? {|x| number % x != 0}
+  if range.all? {|x| number % x != 0} && number > 0
     return true
   else
     return false
