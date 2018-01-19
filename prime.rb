befb8143e7fe14a6f216cc2b0cb5767b7a71a62f
Jennifer Dobak <jdobak1@gmail.com>
2018-01-19 18:01:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -15,12 +15,13 @@ end
 
 prime?(-1)
 
-range = (2..number-1).to_a
-range.all? {|x| number % x != 0} &&
+
+ &&
 =end
 
 def prime?(number)
-  if number > 0
+  range = (2..number-1).to_a
+  if number > 0 && range.all? {|x| number % x != 0}
     return true
   else
     return false
